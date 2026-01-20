1d991b3fef57a58e02dd6802729ed7d38986b48c
Siddharth Upadhyay <siddharthupadhyay@gofynd.com>
2026-01-20 21:13:09 +0530

    added inference for model served via ollama

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ echo "Installing x..."
 # Check if running as root for /usr/local/bin
 if [ "$INSTALL_DIR" = "/usr/local/bin" ] && [ "$(id -u)" -ne 0 ]; then
     echo "This script requires sudo to install to $INSTALL_DIR"
-    echo "Please run: curl -sSL https://raw.githubusercontent.com/arpitbbhayani/x/master/install.sh | sudo sh"
+    echo "Please run: curl -sSL https://raw.githubusercontent.com/sid3945/x/master/install.sh | sudo sh"
     exit 1
 fi
 
 # Download the script
-curl -sSL https://raw.githubusercontent.com/arpitbbhayani/x/master/x -o "$INSTALL_DIR/$SCRIPT_NAME"
+curl -sSL https://raw.githubusercontent.com/sid3945/x/master/x -o "$INSTALL_DIR/$SCRIPT_NAME"
 
 # Make it executable
 chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
